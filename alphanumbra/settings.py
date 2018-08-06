e96b4ce57fdf8050af8c7e310915ba26013b7047
spacerest <robconvo@gmail.com>
2018-08-06 15:36:51 +0200

    update cache

--- alphanumbra/settings.py
+++ alphanumbra/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import __keys__
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*)tx(j#5)*=9r4+n58fj1h_m-vswyv+le^&toh5(l_4*a$^#pa'
-
+SECRET_KEY = __keys__.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
